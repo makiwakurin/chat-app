1fda3281939a9672b68c9150f20cfd078c3d1b46
makiwakurin <takahiro1608@gmail.com>
2020-08-22 00:55:48 +0900

    ログアウト機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,12 @@ class UsersController < ApplicationController
     if current_user.update(user_params)
       #もしユーザーの情報を更新できたら（user＿params:nameと：emailを含んだ情報）
       redirect_to root_path
-      # root_path(シャット画面)へ遷移して
+      # root_path(チャット画面)へ遷移して
     else
       render :edit
       # ユーザーの情報を更新できなかったらrender edit(ユーザー情報編集画面へ遷移して)
     end
+
   end
 
   private
