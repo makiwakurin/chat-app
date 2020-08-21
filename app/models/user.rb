2acb88a25829e4dd6fa035724d272e4d47492467
makiwakurin <takahiro1608@gmail.com>
2020-08-22 01:14:23 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  # nameカラムにpresence: trueを設ける事で空の場合はDBに保存しないと言う
+  # バリデーションを設定しています。 
+#   オブジェクトがDBに保存される前に、そのデータが正しいかどうかを検証する仕組みを
+# バリデーションというpresence: trueは検証結果がtrueであればエラーを出さないよと言うこと
+# つまりnemeカラムが空でないこと
 end
