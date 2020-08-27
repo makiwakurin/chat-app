824179e9e8a9e464d7f6b346d0ea47f9de657053
makiwakurin <takahiro1608@gmail.com>
2020-08-28 00:38:58 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -10,13 +11,17 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
+      # 全てのメッセージ情報に紐づくユーザー情報を
+      # includes(:user)と記述する事でユーザー情報を一度のアクセスでまとめて
+      # 取得できる。
       render :index
     end
   end
 
   private
 
-  def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
-  end
+ def message_params
+   params.require(:message).permit(:content).merge(user_id: current_user.id)
+ end
 end
