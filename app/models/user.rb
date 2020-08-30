12101c7b8d4fa2fd5ed7d55463d05f5fe91c484d
makiwakurin <takahiro1608@gmail.com>
2020-08-30 16:40:09 +0900

    各モデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+         validates :name, presence: true
   # nameカラムにpresence: trueを設ける事で空の場合はDBに保存しないと言う
   # バリデーションを設定しています。 
 #   オブジェクトがDBに保存される前に、そのデータが正しいかどうかを検証する仕組みを
 # バリデーションというpresence: trueは検証結果がtrueであればエラーを出さないよと言うこと
 # つまりnemeカラムが空でないこと
-  has_many :room_users
+        has_many :room_users
   # 一人のuserは一人のユーザーに紐づくroomを複数持っている
-  has_many :rooms, through: :room_users
+        has_many :rooms, through: :room_users
   # 一人userは複数のroomwをもち。経由してroom_usersテーブルで管理する
-  has_many :messages
+        has_many :messages
 end
